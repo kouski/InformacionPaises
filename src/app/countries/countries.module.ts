19e64f6a7bc02fd2b236c89c1a21e40fa3f1ae2d
kouski <72550619+kouski@users.noreply.github.com>
2024-02-15 12:38:46 +0100

    primer commit, modulos countries y shared

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountriesComponent } from './countries.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CountriesComponent]
+})
+export class CountriesModule { }
