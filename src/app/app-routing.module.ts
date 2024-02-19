3899b89f53139b853790ea66b62f9abb06831b0c
kouski <72550619+kouski@users.noreply.github.com>
2024-02-19 11:09:30 +0100

    componentes home, about y contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactComponent } from './shared/pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component:HomePageComponent
+  },
+  {
+    path: 'about',
+    component:AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component:ContactComponent
+  },
+  {
+    path: '**',
+    redirectTo:'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [CommonModule, RouterModule],
+  declarations: [
+    SharedComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactComponent,
   ],
-  declarations: [SharedComponent]
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
